b8c3e85d693b7a81cf89c1f9964df8cc35c27426
MUBASHIR AHMAD MIR <124456653+Mubashir24241@users.noreply.github.com>
2026-04-27 00:38:28 +0530

    Update

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -1,8 +1,4 @@
-#This solution, non-production-ready template describes AWS Codepipeline based CICD Pipeline for terraform code deployment.
-#© 2023 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
-#This AWS Content is provided subject to the terms of the AWS Customer Agreement available at
-#http://aws.amazon.com/agreement or other written agreement between Customer and either
-#Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
+
 
 resource "aws_codebuild_project" "terraform_codebuild_project" {
 
